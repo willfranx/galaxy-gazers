23b785b05c10a2a1362d5874f9cf17176d070ace
William Franklin <frankliw@flip1.engr.oregonstate.edu>
2022-11-09 19:18:19 -0800

    added Node application files and rearranged

---
+++ app.js
@@ -0,0 +1,45 @@
+// App.js
+
+/*
+    SETUP
+*/
+
+var express = require('express');   // We are using the express library for the web server
+var app     = express();            // We need to instantiate an express object to interact with the server in our code
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+PORT        = 8021;                 // Set a port number at the top so it's easy to change in the future
+
+// Database
+var db = require('./database/db-connector')
+
+// app.js
+
+const { engine } = require('express-handlebars');
+var exphbs = require('express-handlebars');     // Import express-handlebars
+app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
+app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
+
+/*
+    ROUTES
+*/
+
+
+// app.js
+
+app.get('/', function(req, res)
+    {
+        let query1 = "SELECT * FROM Astronomers;";               // Define our query
+
+        db.pool.query(query1, function(error, rows, fields){    // Execute the query
+
+            res.render('index', {data: rows});                  // Render the index.hbs file, and also send the renderer
+        })                                                      // an object where 'data' is equal to the 'rows' we
+    });                                                         // received back from the query
+
+/*
+    LISTENER
+*/
+app.listen(PORT, function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
+    console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
+});
